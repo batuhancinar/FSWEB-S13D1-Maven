90e0bc2b156ca766884ec490c283c9665b7c7326
batuhancinar <batuhanfaruk1998@gmail.com>
2024-09-14 13:19:59 +0300

    sprint13ders1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,39 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23 || !isBarking){
+            return false;
+        }
+        return clock >= 20 || clock < 8;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       if((firstAge>13 && firstAge<=19)||(secondAge>13 && secondAge<=19)||(thirdAge>13 && thirdAge<=19)){
+           return true;
+       }
+        else return false;
+
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int ds = 25;
+        int ys = isSummer ? 45 : 35;
+
+        return temp < ys && temp > ds;
     }
 
     public static double area(double width, double height) {
-       
+        if(width < 0 || height < 0){
+        return -1;
+        }
+        return width * height;
     }
 
+
     public static double area(double radius) {
-       
+       if (radius < 0){
+           return -1;
+       }
+       return radius*radius*Math.PI;
     }
 }
